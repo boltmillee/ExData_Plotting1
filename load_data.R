80823cc083f16560d52f4dcf4aa71b80ac68fed8
Abhinav Dixit <boltmillee@gmail.com>
2015-03-09 00:54:17 +0530

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -1,5 +1,5 @@
 ## This file is for loading the large dataset.
-filename <- "./data/household_power_consumption.txt"
+filename <- "c:/html/household_power_consumption.txt"
 data <- read.table(filename,
                    header = TRUE,
                    sep = ";",
